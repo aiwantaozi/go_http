2ddf820bd3402a91144cc7a3934426d7f4f133b8
Michelia <765412321@qq.com>
2016-06-17 16:54:05 +0800

    update controller methods name

--- controllers/ping_handler.go
+++ controllers/ping_handler.go
@@ -5,7 +5,7 @@ import (
 	"net/http"
 )
 
-func PingHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func TestPingHandler(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	okRes := map[string]interface{}{"status": http.StatusOK, "code": 1000, "message": "ok"}
 	ResderResponseInstance(w, okRes)
 }
