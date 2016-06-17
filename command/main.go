359a245f233df5cfbe092aa293092eac304ea936
Michelia <765412321@qq.com>
2016-06-17 16:49:14 +0800

    add common res

--- command/main.go
+++ command/main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
+	"github.com/aiwantaozi/go_http/controllers"
 	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
-	"project/controllers"
 )
 
 func HandleRequests() {
 
 	router := httprouter.New()
 	router.GET("/ping", controllers.PingHandler)
-	log.Fatal(http.ListenAndServe(":8081", router))
+	log.Fatal(http.ListenAndServe(":8082", router))
 }
 
 func main() {
---
+++ controllers/common_response.go
@@ -0,0 +1,54 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"project/pkgdir"
+)
+
+type JsonResponse struct {
+	Status  int    `json:"status"`
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func RenderResponseError(w http.ResponseWriter, responseCode int, responseStatus int, errorMessage string) {
+	errorResponse := JsonResponse{
+		Status:  responseStatus,
+		Code:    responseCode,
+		Message: errorMessage,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(responseStatus)
+	json.NewEncoder(w).Encode(errorResponse)
+}
+
+func ResderResponseInstance(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(data)
+}
+
+func ResderResponseCollection(w http.ResponseWriter, r *http.Request, data interface{}, size int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	var href string
+	if len(r.URL.RawQuery) != 0 {
+		href = r.Host + r.URL.Path + "?" + r.URL.RawQuery
+	} else {
+		href = r.Host + r.URL.Path
+	}
+	res := map[string]interface{}{"href": href, "offset": 0, "size": size, "items": data}
+	json.NewEncoder(w).Encode(res)
+}
+
+func RenderResponseAppError(w http.ResponseWriter, appError *pkgdir.AppError) {
+	errorResponse := JsonResponse{
+		Status:  appError.Status,
+		Code:    appError.Code,
+		Message: appError.Error.Error(),
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(appError.Status)
+	json.NewEncoder(w).Encode(errorResponse)
+}
